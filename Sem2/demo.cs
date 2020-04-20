c930efdd35ea13571fdd5fd6b1d590f0acb51f60
Admin <maithuylinh2196@gmail.com>
2020-04-20 14:57:46 +0700

    demo bb

---
+++ Sem2/demo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sem2
+{
+    public class demo
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello nhan");
+        }
+    }
+}
